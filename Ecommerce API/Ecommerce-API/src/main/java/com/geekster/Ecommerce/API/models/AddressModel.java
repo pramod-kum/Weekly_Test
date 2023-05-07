b4706eaf0eebb2d6a9cb54de1217d78c27d77a5c
pramod-kum <pramodkumar912894@gmail.com>
2023-05-07 15:25:46 +0530

    first commit

---
+++ Ecommerce API/Ecommerce-API/src/main/java/com/geekster/Ecommerce/API/models/AddressModel.java
@@ -0,0 +1,25 @@
+package com.geekster.Ecommerce.API.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class AddressModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int addressId;
+    private String addressName;
+    private String addressLandMark;
+    private String addressPhoneNumber;
+    private String addressZipCode;
+    private String addressState;
+//    @OneToMany
+//    @JoinColumn(name = "address_foreignkey_user")
+//    private UserModel userModel;
+}
